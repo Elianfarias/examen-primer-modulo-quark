009489fd62c367a1921c699ec3dd5d2135cc053d
Elián Farias <elianfarias0@gmail.com>
2022-05-24 10:15:05 -0300

    primer commit de codigo

---
+++ ExamenPrimerEtapaQuark/Models/Electrodomestico.cs
@@ -0,0 +1,34 @@
+﻿using ExamenPrimerEtapaQuark.Models.Enum;
+
+namespace ExamenPrimerEtapaQuark.Models
+{
+    public class Electrodomestico
+    {
+        protected float PrecioBase { get; }
+        protected Color Color { get; }
+        protected bool ConsumoEnergetico { get; }
+        protected float Peso { get; }
+
+        public Electrodomestico()
+        {
+            this.PrecioBase = 100;
+            this.Color = Color.blanco;
+            this.ConsumoEnergetico = false;
+            this.Peso = 5;
+        }
+
+        public Electrodomestico(float precio, float peso)
+        {
+            this.PrecioBase = precio;
+            this.Peso = peso;
+        }
+
+        public Electrodomestico(float precio, Color color, bool consumoEnergetico, float peso)
+        {
+            this.PrecioBase = precio;
+            this.Color = color;
+            this.ConsumoEnergetico = consumoEnergetico;
+            this.Peso = peso;
+        }
+    }
+}
---
+++ ExamenPrimerEtapaQuark/Models/Enum/Color.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenPrimerEtapaQuark.Models.Enum
+{
+    public enum Color
+    {
+        blanco,
+        negro,
+        rojo,
+        azul,
+        gris
+    }
+}
